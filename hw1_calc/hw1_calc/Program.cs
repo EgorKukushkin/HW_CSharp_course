b944b0cc87f765f3ea2f563f9cd1da7eae29c33b
Egor Kukushkin <kukushkin.eiu@phystech.edu>
2026-05-11 22:13:04 +0300

    fix task 1

--- hw1_calc/hw1_calc/Program.cs
+++ hw1_calc/hw1_calc/Program.cs
@@ -1,60 +1,125 @@
-﻿public class Calc()
+public class Calc
 {
     static void Main()
     {
-        for(;;)
+        while (true)
         {
-            Console.WriteLine("Введите первое число (или q чтобы завершить):");
-            string num1_str = Console.ReadLine();
-
-            if (num1_str == "q")
-                break;
-
-            double num1 = Convert.ToDouble(num1_str);
-
-            Console.WriteLine("Введите второе число (или q чтобы завершить):");
-            string num2_str = Console.ReadLine();
-
-            if (num2_str == "q")
-                break;
-
-            double num2 = Convert.ToDouble(num2_str);
-
-            Console.WriteLine("Выберите тип операции (+, -, *, /):");
-            string operation = Console.ReadLine();
-            
-            if (operation == "q")
-                break;
-
-            double res = 0;
-
-            switch (operation)
+            double num1 = 0;
+            bool exit1 = false;
+            while (true)
             {
-                case "+":
-                    res = num1 + num2;
-                    break;
-
-                case "-":
-                    res = num1 - num2;
-                    break;
-
-                case "*":
-                    res = num1 * num2;
-                    break;
-
-                case "/":
-                    if (num2 == 0)
-                    {
-                        Console.WriteLine("Произошло деление на ноль");
-                        continue;
-                    }
-                    res = num1 / num2;
-                    break;
-
-                default:
-                    Console.WriteLine("Неизвестная операция");
+                Console.WriteLine("Введите первое число (или q чтобы завершить):");
+                string num1_str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(num1_str))
+                {
+                    Console.WriteLine("Пустой ввод");
                     continue;
+                }
+                if (num1_str == "q")
+                {
+                    exit1 = true;
+                    break;
+                }
+                if (double.TryParse(num1_str, out num1))
+                {
+                    break;
+                }
+                Console.WriteLine("Неверный формат ввода");
             }
+
+            if (exit1)
+            {
+                break;
+            }
+            
+            double num2 = 0;
+            bool exit2 = false;
+            while (true)
+            {
+                Console.WriteLine("Введите второе число (или q чтобы завершить):");
+                string num2_str = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(num2_str))
+                {
+                    Console.WriteLine("Пустой ввод");
+                    continue;
+                }
+                if (num2_str == "q")
+                {
+                    exit2 = true;
+                    break;
+                }
+                if (double.TryParse(num2_str, out num2))
+                {
+                    break;
+                }
+                Console.WriteLine("Неверный формат ввода");
+            }
+
+            if (exit2)
+            {
+                break;
+            }
+            
+            double res = 0;
+            bool exit3 = false;
+            bool success_flag = false;
+            while (true)
+            {
+                Console.WriteLine("Выберите тип операции (+, -, *, /):");
+                string operation = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(operation))
+                {
+                    Console.WriteLine("Пустой ввод");
+                    continue;
+                }
+                if (operation == "q")
+                {
+                    exit3 = true;
+                    break;
+                }
+                switch (operation)
+                {
+                    case "+":
+                        res = num1 + num2;
+                        success_flag = true;
+                        break;
+
+                    case "-":
+                        res = num1 - num2;
+                        success_flag = true;
+                        break;
+
+                    case "*":
+                        res = num1 * num2;
+                        success_flag = true;
+                        break;
+
+                    case "/":
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("Произошло деление на ноль");
+                            continue;
+                        }
+                        res = num1 / num2;
+                        success_flag = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Неизвестная операция");
+                        continue;
+                }
+
+                if (success_flag)
+                {
+                    break;
+                }
+            }
+
+            if (exit3)
+            {
+                break;
+            }
+            
 
             Console.WriteLine($"Результат: {res}");
             Console.WriteLine();
